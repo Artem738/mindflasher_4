0c3297d71d592a992a7af3e9923bcf4790f97769
Artem <artembabichev@gmail.com>
2024-08-07 23:30:36 +0300

    good auth v0.0.2777 full login done 1

---
+++ lib/models/deck_model.dart
@@ -0,0 +1,11 @@
+class Deck {
+  final int id;
+  final String name;
+  final String description;
+
+  Deck({
+    required this.id,
+    required this.name,
+    required this.description,
+  });
+}
